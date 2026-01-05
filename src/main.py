11651bb9bffeb2c4feb01d36a591ae49ba6cb281
Yassine El-jdid <eljdidyassine@gmail.com>
2026-01-05 01:11:13 +0100

    added chat container and llm

--- src/main.py
+++ src/main.py
@@ -1,21 +1,57 @@
-"""Main entry point for the Streamlit application."""
+from dotenv import load_dotenv
+load_dotenv()
 
 import streamlit as st
+from groq import Groq
 
-# Page configuration
-st.set_page_config(
-    page_title="RAG Agent",
-    page_icon="🤖",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+client = Groq()
+def generate_response(query: str):
+    stream = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": query,
+            }
+        ],
+        model="llama-3.1-8b-instant",
+        temperature=0.5,
+        max_completion_tokens=1024,
+        top_p=1,
+        stop=None,
+        stream=True,
+    )
+
+    for chunk in stream:
+        chunk_content = chunk.choices[0].delta.content
+        yield chunk_content if chunk_content is not None else ""
+
+def chat_interface():
+    new_chat = st.sidebar.button("New Chat", type="primary")
+    if "messages" not in st.session_state or new_chat:
+        st.session_state.messages = []
+    
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+    
+    if query := st.chat_input("What is up?"):
+        with st.chat_message("user"):
+            st.write(query)
+
+        st.session_state.messages.append({"role": "user", "content": query})
+
+        with st.chat_message("assistant"):
+            response = st.write_stream(generate_response(query))
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
 
 def main():
-    """Main application function."""
-    st.title("🤖 RAG Agent")
-    st.write("Welcome to the RAG Agent application!")
-    
-    # Add your main application code here
+    chat_interface()
+
 
 if __name__ == "__main__":
     main()
